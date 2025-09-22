fba695911cc58dcb244a0281443ec9c07c6eb8ae
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-09-22 11:37:22 +0300

    pushes dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the main element with id 'main'
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create an h1 element with id 'victory' and assign it to newHeader variable
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the innerHTML to contain "BLACKBOXAI is the champion"
+newHeader.innerHTML = 'BLACKBOXAI is the champion';
+
+// Append the newHeader to the body
+document.body.append(newHeader);
